12e38c6db94e6223d6261de6ab8c5e72e57a5ec9
shikha-igtm <imca2424@rajagiri.edu>
2024-10-19 10:47:03 +0530

    Add files via upload

---
+++ arithmetic operation.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int num1 ,num2;
+	int sum,sub,mult,mod;
+	float div;
+	printf("enter any  two numbers");
+	scanf("%d%d", &num1, &num2);
+	sum = num1+num2;
+	sub = num1-num2;
+	div = (float)num1/num2;
+	printf("SUM = %d\n",sum);
+	printf("DIFFERENCE = %d\n",sub);
+	printf("PRODUCT = %d\n",mult);
+	printf("QUOTIENT= %f\n",div);
+	printf("MODULUS= %d",mod);
+	return 0;
+}
