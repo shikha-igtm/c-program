14987ca81af8a5dace406c5744d7890ad03e3a1e
shikha-igtm <imca2424@rajagiri.edu>
2024-10-25 12:32:41 +0530

    Add files via upload

---
+++ largest three numbers.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int max_of_three(int a, int b,int c)
+	{
+       return(>=b&&a>=c)?a:b(b>=c?b:c);
+	}
+	int main()
+	{
+	int num1,num2num3;
+	printf("enter three number");
+	scanf("%d%d%d",&num1 &num2 ,&num3);
+    }
+	int maximum= max_of_three(num1,num2,num3);
+	printf("the maximum number is:%d\n",maximum);
+	return 0;
+}
